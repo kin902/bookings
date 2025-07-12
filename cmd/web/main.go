72d960c9e8de0dff856e8cade2846b35f8e82c80
Kin Loki <kinquay@gmail.com>
2025-07-12 11:14:54 +0700

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/alexedwards/scs/v2"
+	"log"
+	"myweb/cmd/pkg/config"
+	"myweb/cmd/pkg/handlers"
+	"myweb/cmd/pkg/render"
+	"net/http"
+	"time"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main application function
+func main() {
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		fmt.Println("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplate(&app)
+
+	//http.HandleFunc("/", handlers.Repo.Home)
+	//http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	//_ = http.ListenAndServe(portNumber, nil)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module myweb
+
+go 1.24.4
+
+require (
+	github.com/go-chi/chi/v5 v5.2.2
+	github.com/justinas/nosurf v1.2.0
+)
+
+require github.com/alexedwards/scs/v2 v2.9.0 // indirect
